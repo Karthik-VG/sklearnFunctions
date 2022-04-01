104c10d5663e929da3d498c446b696a694fcc8de
Karthik-VG <karthikvg.engineer@gmail.com>
2022-04-01 22:32:49 +0530

    minor enhacement

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ USER_NAME = "Karthik-VG"
 
 setuptools.setup(
     name= f"{PROJECT_NAME}",
-    version="0.0.1",
+    version="0.0.2",
     author=USER_NAME,
     author_email="karthikvg.engineer@gmail.com",
     description="module built on needy sklearn preprocessing functions",
